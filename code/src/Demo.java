4bfb9e5670f4ab6a60348e3002603fd6a2241bfc
lsm <1057702942@qq.com>
2020-04-23 14:34:38 +0800

    leetcode 105 add

--- code/src/Demo.java
+++ code/src/Demo.java
@@ -56,7 +56,38 @@ public class Demo {
      * 104. 二叉树的最大深度
      */
     public int maxDepth(TreeNode root) {
-        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
+    /**
+     * 105. 从前序与中序遍历序列构造二叉树
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0 || inorder == null || inorder.length == 0 || preorder.length != inorder.length) {
+            return null;
+        }
+        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode buildTree(int[] preorder, int pStart, int pEnd, int[] inorder, int iStart, int iEnd) {
+        //递归的第一步：递归终止条件，避免死循环
+        if (pStart > pEnd || iStart > iEnd) {
+            return null;
+        }
+        //重建根节点
+        TreeNode treeNode = new TreeNode(preorder[pStart]);
+        //index找到根节点在中序遍历的位置
+        int index = 0;
+        while (inorder[iStart + index] != preorder[pStart]) {
+            index++;
+        }
+        //重建左子树
+        treeNode.left = buildTree(preorder, pStart + 1, pStart + index, inorder, iStart, iStart + index - 1);
+        //重建右子树
+        treeNode.right = buildTree(preorder, pStart + index + 1, pEnd, inorder, iStart + index + 1, iEnd);
+        return treeNode;
+    }
 }
