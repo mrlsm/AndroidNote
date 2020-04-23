2b9efec75396021355fdd354bd5874a70fce1542
mrlsm <1057702942@qq.com>
2020-04-23 21:00:28 +0800

    leetcode  101  submit

--- code/src/Demo.java
+++ code/src/Demo.java
@@ -104,4 +104,20 @@ public class Demo {
         t1.right = mergeTrees(t1.right, t2.right);
         return t1;
     }
+
+    /**
+     * 101. 对称二叉树
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return isSame(root, root);
+    }
+
+    private boolean isSame(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        return (left.val == right.val)
+                && isSame(left.right, right.left)
+                && isSame(left.left, right.right);
+    }
+
 }
