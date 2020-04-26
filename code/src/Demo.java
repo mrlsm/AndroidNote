0e0101e6018cc4be092331178d117f56e515747e
mrlsm <1057702942@qq.com>
2020-04-26 20:52:35 +0800

    leetcode  543  commit

--- code/src/Demo.java
+++ code/src/Demo.java
@@ -148,4 +148,20 @@ public class Demo {
                 && isSame(left.left, right.right);
     }
 
+
+    private int ans;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+
+    public int depth(TreeNode node) {
+        if (node == null) return 0;
+        int L = depth(node.left);
+        int R = depth(node.right);
+        ans = Math.max(ans, L + R + 1);
+        return Math.max(L, R) + 1;
+    }
 }
