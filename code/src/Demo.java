735c228a264a87707a89b74461afdc051b365be2
lsm <1057702942@qq.com>
2020-04-28 18:17:07 +0800

    leetcode 21 commit

--- code/src/Demo.java
+++ code/src/Demo.java
@@ -248,4 +248,29 @@ public class Demo {
         }
         return num;
     }
+
+    /**
+     * 21. 合并两个有序链表
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 == null) {
+            cur.next = l2;
+        } else {
+            cur.next = l1;
+        }
+        return dummyHead.next;
+    }
 }
