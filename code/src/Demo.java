3277a632fb2164e1a746b9d544b2873699628e86
lsm <1057702942@qq.com>
2020-04-30 11:09:57 +0800

    leetcode 160 commit

--- code/src/Demo.java
+++ code/src/Demo.java
@@ -316,4 +316,17 @@ public class Demo {
         Sum(root.left, sum);
         Sum(root.right, sum);
     }
+
+    /**
+     * 160. 相交链表
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
 }
