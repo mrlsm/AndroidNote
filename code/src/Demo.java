a92378b00f8c63735cc12e44062cd6a6104542d4
lsm <1057702942@qq.com>
2020-05-05 14:11:27 +0800

    leetcode 98 commit

--- code/src/Demo.java
+++ code/src/Demo.java
@@ -343,4 +343,25 @@ public class Demo {
         }
         return max;
     }
+
+    /**
+     * 98. 验证二叉搜索树
+     */
+    long cur = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // 访问左子树
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        // 访问当前节点：如果当前节点小于等于中序遍历的前一个节点，说明不满足BST，返回 false；否则继续遍历。
+        if (root.val <= cur) {
+            return false;
+        }
+        cur = root.val;
+        // 访问右子树
+        return isValidBST(root.right);
+    }
 }
